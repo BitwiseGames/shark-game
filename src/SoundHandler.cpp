2e3b8ae0d359f78300af41fa85e24e11e50015e8
Luke McColloch <luke.mcc292@gmail.com>
2015-07-20 21:00:49 -0400

    sound handler update

--- src/SoundHandler.cpp
+++ src/SoundHandler.cpp
@@ -11,18 +11,33 @@ SoundHandler* SoundHandler::getTheInstance(){
 
 bool SoundHandler::load(string fileName, string ID, soundType type){
   if (type == SOUND_MUSIC){
+    Mix_Music* musicFile = Mix_LoadMUS(fileName.c_str());
+    if (musicFile == nullptr){
+      cout << "Could not load music file: " << fileName << endl << Mix_GetError() << endl;
+      return false;
+    }
+
+    music[ID] = musicFile;
+    return true;
 
   }
   else if (type == SOUND_EFFECT){
+    Mix_Chunk* effectFile = Mix_LoadWAV(fileName.c_str());
+    if (effectFile == nullptr){
+      cout << "Could not load sound effect file: " << fileName << endl << Mix_GetError() << endl;
+      return false;
+    }
 
+    soundEffects[ID] = effectFile;
+    return true;
   }
   return false;
 }
 
-void SoundHandler::playMusic(){
-
+void SoundHandler::playMusic(string ID, int loop){
+  Mix_PlayMusic(music[ID], loop);
 }
 
-void SoundHandler::playSound(){
-
+void SoundHandler::playSound(string ID, int loop){
+  Mix_PlayChannel(-1, soundEffects[ID], loop);
 }
