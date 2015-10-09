9ff3051ccbe7dd4c488555057b54bfde9fc41532
Laggy44 <luke.mcc292@gmail.com>
2015-10-08 22:55:24 -0500

    jellyfish
    if the player collides with the jellyfish's bell, he bounces backwards.
    If he hits the tentacles, he takes damage

--- include/Jellyfish.h
+++ include/Jellyfish.h
@@ -3,15 +3,41 @@
 
 #include "Enemy.h"
 #include "GameObjectFactory.h"
+#include "CollisionHandler.h"
+#include <iostream>
 #include <cmath>
 
 class Jellyfish : public Enemy {
     public:
-        Jellyfish() { birthTime = SDL_GetTicks();};
+
+        Jellyfish() {
+            birthTime = SDL_GetTicks();
+        };
+
         void update(){
             float timeDiff = (SDL_GetTicks() - birthTime) / 100.0f;
             position.setY(spawnY + sin(timeDiff) * Game::getTheInstance()->getDeltaTime() * 1000);
         }
+
+        void playerCollide(){
+            int py = CollisionHandler::getTheInstance()->getPlayer()->Position()->Y();
+            if (py > position.Y() + BELL_HEIGHT){ // player is hitting the tentacles
+                int playerHealth = CollisionHandler::getTheInstance()->getPlayer()->Health() - 1;
+                CollisionHandler::getTheInstance()->getPlayer()->setHealth(playerHealth);
+            }
+            else { // player is hitting the bell
+                int px = CollisionHandler::getTheInstance()->getPlayer()->Position()->X();
+                int newX;
+                if (px > position.X()){ // hitting from the right
+                    newX = CollisionHandler::getTheInstance()->getPlayer()->Position()->X() + PUSH_DISTANCE;
+                }
+                else { // from the left
+                    newX = CollisionHandler::getTheInstance()->getPlayer()->Position()->X() - PUSH_DISTANCE;
+                }
+                CollisionHandler::getTheInstance()->getPlayer()->setPosition(Vector2D{newX, py});
+            }
+        }
+
         void load(int x, int y, int w, int h, int frames, int cID, int _health, int speed, string tID){
             position.setX(x);
             position.setY(y);
@@ -32,6 +58,10 @@ class Jellyfish : public Enemy {
     private:
         int birthTime;
         int spawnY;
+        const int BELL_HEIGHT = 54;
+        const int PUSH_DISTANCE = 50;
+        SDL_Rect bell;
+        SDL_Rect tentacles;
 };
 
 class JellyfishCreator : public BaseCreator {
